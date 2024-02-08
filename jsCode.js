580fea3e2e77246390ff6046551f8e32c2a0a92f
Flydistrito36 <145552502+Flydistrito36@users.noreply.github.com>
2024-02-08 15:21:37 -0500

    Add files via upload

---
+++ jsCode.js
@@ -0,0 +1,42 @@
+
+// JavaScript Document 
+
+$ (document).ready(paginaOK);
+function paginaOK()
+
+{
+
+$("#iconocel").click(mostrarMenu);}
+function mostrarMenu()
+{
+$("#menucel").slideToggle(1000); 
+}
+
+
+$("#foto1").click(accion1);
+$("#foto2").click(accion2);
+$("#foto3").click(accion3);
+
+
+
+
+function mostrarMenu()
+{
+$("#menucel").slideToggle(1000); 
+}
+
+
+function accion1()
+{
+$("#parrafo1").slideToggle(1000);  
+}
+
+function accion2()
+{
+    $("#parrafo2").slideToggle(1000);  
+    }
+   
+function accion3() 
+    {
+        $("#parrafo3").slideToggle(1000);  
+        }
